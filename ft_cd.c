ac7f3ca0ea287503773a8cc3b57e27c18d1a361e
Renaud Monney <rmonney@c1r14s7.42lausanne.ch>
2022-04-12 17:06:33 +0200

    merge 2 paske je fais de la mert

---
+++ ft_cd.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_cd.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nhirzel <marvin@42lausanne.ch>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/10 19:53:20 by nhirzel           #+#    #+#             */
+/*   Updated: 2022/04/12 17:04:38 by rmonney          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "minishell.h"
+
+static void	update_oldpwd(char **env, char *oldpwd)
+{
+	int		i;
+	char	*temp;
+
+	i = 0;
+	while (env[i])
+	{
+		if (ft_strncmp(env[i], "OLDPWD=", 7) == 0)
+		{
+			temp = ft_strjoin("OLDPWD=", oldpwd);
+			env[i] = temp;
+		}
+		i++;
+	}
+}
+
+static void	update_pwd(char **env)
+{
+	int		i;
+	char	*temp;
+	char	pwd[4097];
+
+	i = 0;
+	while (env[i])
+	{
+		if (ft_strncmp(env[i], "PWD=", 4) == 0)
+		{
+			getcwd(pwd, 4096);
+			temp = ft_strjoin("PWD=", pwd);
+			env[i] = temp;
+		}
+		i++;
+	}
+}
+
+void	ft_cd(t_para *para)
+{
+	char	temp_pwd[4097];
+	char	*path;
+
+	getcwd(temp_pwd, 4096);
+	if (para->out[3] == '\0')
+		path = ("/Users");
+	else
+	{
+		while (*para->out != ' ')
+			para->out++;
+		para->out++;
+		path = para->out;
+	}
+	if (chdir(path) == 0)
+	{
+		update_oldpwd(para->env, temp_pwd);
+		update_pwd(para->env);
+	}
+	else
+//		printf("Error : %s\n", strerror(errno));
+		error_handler(7, para);
+}
